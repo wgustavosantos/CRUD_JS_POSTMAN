c94a60e7e4e5f7bef14ab20fc0e86ec9a63ce803
Wenderson Santos <guto15santos@gmail.com>
2022-05-24 13:42:29 -0300

    Configurando dotenv e mondodb

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const { Person } = require("./person");
-//require("./modules/http");
+const dotenv = require("dotenv");   //estabelecer conexão com o bd
 let person = new Person("Gustavo");
 
+dotenv.config(); //Para acessar variaveis de ambiente  
+//require("./modules/http");
 //require("./modules/fs") //importando arquivo e executando 
 //console.log(person.sayMyName());
 
---
+++ src/database/connect.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const connectToDatabase = async () => {
+    await mongoose.connect(`mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@nodejs-dicasparadev.sdfft.mongodb.net/dicasfordev?retryWrites=true&w=majority`, (error) => {
+        if (error){
+            console.log("Ocorreu um erro ao se conectar com o Banco de Dados: ", error);
+        } else {
+            console.log("Conexão ao banco de dados realizada com sucesso");
+        }
+    })
+};
+
+module.exports = connectToDatabase 
