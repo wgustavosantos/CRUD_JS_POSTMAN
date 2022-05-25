7c9ee8b6d75872286afac0ace5c34d1ae5e5bcd1
Wenderson Santos <guto15santos@gmail.com>
2022-05-25 17:57:30 -0300

    CRUD completo

--- modules/express.js
+++ modules/express.js
@@ -9,8 +9,6 @@ app.get("/home", (req, res) => {
   res.status(200).send("<h1>Hello Word com NodeJs usando Express</h1>");
 });
 
-
-
 app.post("/users", async (req, res) => {
   try {
     const user = await UserModel.create(req.body);
@@ -22,16 +20,13 @@ app.post("/users", async (req, res) => {
 
 app.get("/users/:id", async (req, res) => {
   try {
-    
     const id = req.params.id;
     const user = await UserModel.findById(id);
     res.status(200).json(user);
-
-
   } catch (error) {
     res.status(500).end(error.message);
   }
-})
+});
 
 app.get("/users", async (req, res) => {
   try {
@@ -40,7 +35,31 @@ app.get("/users", async (req, res) => {
   } catch (error) {
     res.status(500).send(error.message); // send pois é string e não json
   }
-})
+});
+
+app.delete("/users/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    await UserModel.findByIdAndDelete(id);
+    console.log("Deletado com sucesso");
+    res.status(200).end;
+  } catch (error) {
+    res.status(500).send(error.message);
+    console.log("Erro: ", error);
+  }
+});
+
+app.put("/users/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const user = await UserModel.findByIdAndUpdate(id, req.body, { new: true });
+    console.log("Usuario atualizado com sucesso.");
+    res.status(500).json(user);
+  } catch (error) {
+    res.status(500).send(error.message);
+    console.log("Erro ao atualizar: ", error);
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Rodando server com Express na porta ${PORT}`);
